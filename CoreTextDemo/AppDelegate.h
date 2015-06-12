73996b9cc556e1e2f86b7f0a5ec86130cdc0c655
Alta <Alta@Users-MacBook-Pro.local>
2015-06-12 20:54:45 +0800

    Initial Commit

---
+++ CoreTextDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreTextDemo
+//
+//  Created by Alta on 15/6/12.
+//  Copyright (c) 2015年 Alta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreTextDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreTextDemo
+//
+//  Created by Alta on 15/6/12.
+//  Copyright (c) 2015年 Alta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
