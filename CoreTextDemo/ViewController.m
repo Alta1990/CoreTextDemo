ffd996fe6954788d761469b8adfc8f96b59f33f9
Alta1990 <hhf901222@163.com>
2015-07-31 21:05:56 +0800

    fix-90

--- CoreTextDemo/ViewController.m
+++ CoreTextDemo/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSString *str=@"dfcvbnghghg";
+    NSString *str_close=@"hjhu";
     // Do any additional setup after loading the view, typically from a nib.
 }
 
